e5119c1e77e142dc63c4722f76756fa44ce4b73a
Alex Clemmer <clemmer.alexander@gmail.com>
2018-03-17 16:13:26 -0700

    Add comments to example

--- examples/queries/subjectsWithSecretAccess.ts
+++ examples/queries/subjectsWithSecretAccess.ts
@@ -4,10 +4,14 @@ const rbac = rbacAuthorization
 const c = Client.fromFile(<string>process.env.KUBECONFIG);
 const subjectsWithSecretAccess = c.rbacAuthorization.v1beta1.Role
   .list()
+  // Find Roles that apply to `core.v1.Secret`. Note the empty string denotes
+  // the `core` namespace.
   .filter(role => rbac.v1beta1.role.appliesTo(role, "", "secrets"))
   .flatMap(role => {
     return c.rbacAuthorization.v1beta1.RoleBinding
       .list()
+      // Find RoleBindings that apply to `role`. Project to a list of subjects
+      // (e.g., Users) `role` is bound to.
       .filter(binding =>
         rbac.v1beta1.roleBinding.referencesRole(binding, role.metadata.name))
       .flatMap(binding => binding.subjects)
