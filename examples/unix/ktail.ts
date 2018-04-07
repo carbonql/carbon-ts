a22c6bda4b24b19090e7799b165cfa723871a76f
Alex Clemmer <clemmer.alexander@gmail.com>
2018-04-06 23:02:35 -0700

    Make ktail suitable for libraries

--- examples/unix/ktail.ts
+++ examples/unix/ktail.ts
@@ -46,32 +46,44 @@ const currNs = argv["all-namespaces"]
   ? undefined
   : c.kubeConfig.getCurrentContextObject().namespace || "default";
 
-c.core.v1.Pod
-  .list(currNs)
-  .flatMap(pod => {
-    // Ignore pod if it doesn't match the regex.
-    if (!podRegex.test(pod.metadata.name)) return [];
+export const ktail = (
+  ns: string | undefined, podRegex: RegExp, stream: boolean,
+): query.Observable<{
+  name: string;
+  logs: string[];
+}> => {
+  return c.core.v1.Pod
+    .list(ns)
+    .flatMap(pod => {
+      // Ignore pod if it doesn't match the regex.
+      if (!podRegex.test(pod.metadata.name)) return [];
 
-    // Get a log stream if `--stream` was passed in, else just get the output of
-    // the standard `logs` request.
-    const logs =
-      stream
-      ? c.core.v1.Pod.logStream(pod.metadata.name, pod.metadata.namespace)
-      : c.core.v1.Pod.logs(pod.metadata.name, pod.metadata.namespace);
+      // Get a log stream if `--stream` was passed in, else just get the output of
+      // the standard `logs` request.
+      const logs =
+        stream
+        ? c.core.v1.Pod.logStream(pod.metadata.name, pod.metadata.namespace)
+        : c.core.v1.Pod.logs(pod.metadata.name, pod.metadata.namespace);
 
-    // For each particular stream of logs, emit output in windowed intervals of
-    // 1 second. This makes the logs slightly more contiguous, so that a bunch
-    // of logs from one pod end up output together.
-    return logs
-      .filter(logs => logs != null)
-      .window(query.Observable.timer(0, 1000))
-      .flatMap(window =>
-        window
-          .toArray()
-          .flatMap(logs => logs.length == 0 ? [] : [logs]))
-      .map(logs => {return {name: pod.metadata.name, logs}});
-  })
-  .forEach(({name, logs}) => {
-    console.log(`${chalk.default.green(name)}:`);
-    logs.forEach(line => console.log(`${line}`))
-  });
+      // For each particular stream of logs, emit output in windowed intervals of
+      // 1 second. This makes the logs slightly more contiguous, so that a bunch
+      // of logs from one pod end up output together.
+      return logs
+        .filter(logs => logs != null)
+        .window(query.Observable.timer(0, 1000))
+        .flatMap(window =>
+          window
+            .toArray()
+            .flatMap(logs => logs.length == 0 ? [] : [logs]))
+        .map(logs => {return {name: pod.metadata.name, logs}});
+    });
+}
+
+// Run query if this module is used as an executable (rather than a library).
+if (require.main === module) {
+  ktail(currNs, podRegex, stream)
+    .forEach(({name, logs}) => {
+      console.log(`${chalk.default.green(name)}:`);
+      logs.forEach(line => console.log(`${line}`))
+    });
+}
