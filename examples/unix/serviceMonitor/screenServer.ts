c9b565290cb1057194dc20a5ea998a3ba21fc3af
Alex Clemmer <clemmer.alexander@gmail.com>
2018-04-16 15:30:47 -0700

    Add DNS table to svcmon

--- examples/unix/serviceMonitor/screenServer.ts
+++ examples/unix/serviceMonitor/screenServer.ts
@@ -99,44 +99,7 @@ var screen = blessed.screen()
 
 var grid = new contrib.grid({rows: 12, cols: 12, screen: screen})
 
-// /**
-//  * Donut Options
-//   self.options.radius = options.radius || 14; // how wide is it? over 5 is best
-//   self.options.arcWidth = options.arcWidth || 4; //width of the donut
-//   self.options.yPadding = options.yPadding || 2; //padding from the top
-//  */
-// var donut = grid.set(8, 8, 4, 2, contrib.donut,
-//   {
-//   label: 'Percent Donut',
-//   radius: 16,
-//   arcWidth: 4,
-//   yPadding: 2,
-//   data: [{label: 'Storage', percent: 87}]
-// })
-
-// var latencyLine = grid.set(8, 8, 4, 2, contrib.line,
-//   { style:
-//     { line: "yellow"
-//     , text: "green"
-//     , baseline: "black"}
-//   , xLabelPadding: 3
-//   , xPadding: 5
-//   , label: 'Network Latency (sec)'})
-
-// var gauge = grid.set(8, 10, 2, 2, contrib.gauge, {label: 'Storage', percent: [80,20]})
 var gauge_two = grid.set(2, 9, 2, 3, contrib.gauge, {label: 'Deployment Progress', percent: 80})
-
-// var sparkline = grid.set(10, 10, 2, 2, contrib.sparkline,
-//   { label: 'Throughput (bits/sec)'
-//   , tags: true
-//   , style: { fg: 'blue', titleFg: 'white' }})
-
-// var bar = grid.set(4, 6, 4, 3, contrib.bar,
-//   { label: 'Server Utilization (%)'
-//   , barWidth: 4
-//   , barSpacing: 6
-//   , xOffset: 2
-//   , maxHeight: 9})
 
 var servicesTable =  grid.set(0, 0, 4, 6, contrib.table,
   { keys: true
@@ -212,14 +175,13 @@ var lcdLineOne = grid.set(0,9,2,3, contrib.lcd,
   }
 );
 
-var errorsLine = grid.set(0, 6, 4, 3, contrib.line,
-  { style:
-    { line: "red"
-    , text: "white"
-    , baseline: "black"}
-  , label: 'Errors Rate'
-  , maxY: 60
-  , showLegend: true })
+var dnsTable =  grid.set(0, 6, 4, 3, contrib.table,
+  { keys: true
+  , fg: 'white'
+  , label: 'DNS entries in namespace'
+  , interactive: false
+  , columnSpacing: 1
+  , columnWidth: [10, 3] });
 
 var transactionsLine = grid.set(4, 9, 4, 3, contrib.line,
           { showNthLabel: 5
@@ -227,8 +189,6 @@ var transactionsLine = grid.set(4, 9, 4, 3, contrib.line,
           , label: 'Total Transactions'
           , showLegend: true
           , legend: {width: 10}})
-
-// var map = grid.set(8, 6, 4, 2, contrib.map, {label: 'Servers Location'})
 
 const makeLog = () => {
   return grid.set(8, 0, 4, 12, contrib.log,
@@ -243,15 +203,6 @@ var log = makeLog();
 var servers = ['US1', 'US2', 'EU1', 'AU1', 'AS1', 'JP1']
 var commands = ['grep', 'node', 'java', 'timer', '~/ls -l', 'netns', 'watchdog', 'gulp', 'tar -xvf', 'awk', 'npm install']
 
-
-// //set dummy data on gauge
-// var gauge_percent = 0
-// setInterval(function() {
-//   gauge.setData([gauge_percent, 100-gauge_percent]);
-//   gauge_percent++;
-//   if (gauge_percent>=100) gauge_percent = 0
-// }, 200)
-
 var gauge_percent_two = 0
 setInterval(function() {
   gauge_two.setData(gauge_percent_two);
@@ -259,62 +210,7 @@ setInterval(function() {
   if (gauge_percent_two>=100) gauge_percent_two = 0
 }, 200);
 
-
-// //set dummy data on bar chart
-// function fillBar() {
-//   var arr = []
-//   for (var i=0; i<servers.length; i++) {
-//     arr.push(Math.round(Math.random()*10))
-//   }
-//   bar.setData({titles: servers, data: arr})
-// }
-// fillBar()
-// setInterval(fillBar, 2000)
-
 servicesTable.focus()
-
-// //set log dummy data
-// setInterval(function() {
-//    var rnd = Math.round(Math.random()*2)
-//    if (rnd==0) log.log('starting process ' + commands[Math.round(Math.random()*(commands.length-1))])
-//    else if (rnd==1) log.log('terminating server ' + servers[Math.round(Math.random()*(servers.length-1))])
-//    else if (rnd==2) log.log('avg. wait time ' + Math.random().toFixed(2))
-//    screen.render()
-// }, 500)
-
-
-// //set spark dummy data
-// var spark1 = [1,2,5,2,1,5,1,2,5,2,1,5,4,4,5,4,1,5,1,2,5,2,1,5,1,2,5,2,1,5,1,2,5,2,1,5]
-// var spark2 = [4,4,5,4,1,5,1,2,5,2,1,5,4,4,5,4,1,5,1,2,5,2,1,5,1,2,5,2,1,5,1,2,5,2,1,5]
-
-// refreshSpark()
-// setInterval(refreshSpark, 1000)
-
-// function refreshSpark() {
-//   spark1.shift()
-//   spark1.push(Math.random()*5+1)
-//   spark2.shift()
-//   spark2.push(Math.random()*5+1)
-//   sparkline.setData(['Server1', 'Server2'], [spark1, spark2])
-// }
-
-
-
-// //set map dummy markers
-// var marker = true
-// setInterval(function() {
-//    if (marker) {
-//     map.addMarker({"lon" : "-79.0000", "lat" : "37.5000", color: 'yellow', char: 'X' })
-//     map.addMarker({"lon" : "-122.6819", "lat" : "45.5200" })
-//     map.addMarker({"lon" : "-6.2597", "lat" : "53.3478" })
-//     map.addMarker({"lon" : "103.8000", "lat" : "1.3000" })
-//    }
-//    else {
-//     map.clearMarkers()
-//    }
-//    marker =! marker
-//    screen.render()
-// }, 1000)
 
 //set line charts dummy data
 
@@ -332,29 +228,17 @@ var transactionsData1 = {
    y: [0, 5, 5, 10, 10, 15, 20, 30, 25, 30, 30, 20, 20, 30, 30, 20, 15, 15, 19, 25, 30, 25, 25, 20, 25, 30, 35, 35, 30, 30]
 }
 
-var errorsData = {
-   title: 'server 1',
-   x: ['00:00', '00:05', '00:10', '00:15', '00:20', '00:25'],
-   y: [30, 50, 70, 40, 50, 20]
-}
-
 var latencyData = {
    x: ['t1', 't2', 't3', 't4'],
    y: [5, 1, 7, 5]
 }
 
 setLineData([transactionsData, transactionsData1], transactionsLine)
-setLineData([errorsData], errorsLine)
-// setLineData([latencyData], latencyLine)
 
 setInterval(function() {
    setLineData([transactionsData, transactionsData1], transactionsLine)
    screen.render()
 }, 500)
-
-setInterval(function() {
-    setLineData([errorsData], errorsLine)
-}, 1500)
 
 setInterval(function(){
   var colors = ['green','magenta','cyan','red','blue'];
@@ -370,23 +254,6 @@ setInterval(function(){
 }, 1500);
 
 var pct = 0.00;
-
-// function updateDonut(){
-//   if (pct > 0.99) pct = 0.00;
-//   var color = "green";
-//   if (pct >= 0.25) color = "cyan";
-//   if (pct >= 0.5) color = "yellow";
-//   if (pct >= 0.75) color = "red";
-//   donut.setData([
-//     {percent: parseFloat(<any>((pct+0.00) % 1)).toFixed(2), label: 'storage', 'color': color}
-//   ]);
-//   pct += 0.01;
-// }
-
-// setInterval(function() {
-//    updateDonut();
-//    screen.render()
-// }, 500)
 
 function setLineData(mockData: any, line: any) {
   for (var i=0; i<mockData.length; i++) {
@@ -406,17 +273,12 @@ screen.key(['escape', 'q', 'C-c'], function() {
 
 // fixes https://github.com/yaronn/blessed-contrib/issues/10
 screen.on('resize', function() {
-  // donut.emit('attach');
-  // gauge.emit('attach');
   gauge_two.emit('attach');
-  // sparkline.emit('attach');
-  // bar.emit('attach');
   servicesTable.emit('attach');
   podsTable.emit('attach');
+  dnsTable.emit('attach');
   lcdLineOne.emit('attach');
-  errorsLine.emit('attach');
   transactionsLine.emit('attach');
-  // map.emit('attach');
   log.emit('attach');
 });
 
